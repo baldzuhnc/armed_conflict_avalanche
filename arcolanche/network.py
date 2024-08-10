793ae1dad9b34db8e0f20f42e59f5d879c2331b6
clemens <clemens@baldzuhn.de>
2024-08-10 23:14:59 +0200

    made mutual information tuple ready

--- arcolanche/network.py
+++ arcolanche/network.py
@@ -42,10 +42,155 @@ def get_adjacency_matrix(neighbor_info_dataframe, time_series):
     return adjacency_matrix
 
 
+def get_adjancency_matrix_mi(neighbor_info_dataframe, time_series, mi_threshold):
+    
+    # mi connections
+    highest_mi_tuples = calculate_mi_tuples(time_series, mi_threshold)
+    
+    # Initialize an empty sparse adjacency matrix of nxn
+    n = len(neighbor_info_dataframe)
+
+    adjacency_matrix = lil_matrix((n, n), dtype=int) #list of lists format
+
+    cell_id_to_position = {cell_id: pos for pos, cell_id in enumerate(neighbor_info_dataframe.index)}
+
+    #iterate over (column name, series), fill adjacency matrix
+    for cell_id, neighbors in neighbor_info_dataframe['neighbors'].items():
+        #current cell_id has to be in time_series (not in ts if zero activity)
+        if cell_id in time_series.columns:
+            for neighbor in neighbors:
+                #neighbour has to be in polygons and time series
+                if neighbor in cell_id_to_position and neighbor in time_series.columns: 
+                    #at position of cell_id and neighbor, set value to 1 -> iteratively fill adjacency matrix
+                    adjacency_matrix[cell_id_to_position[cell_id], cell_id_to_position[neighbor]] = 1
+    
+    # Update the adjacency matrix with MI connections
+    for cell_id1, cell_id2 in highest_mi_tuples:
+        if cell_id1 in cell_id_to_position and cell_id2 in cell_id_to_position:
+            adjacency_matrix[cell_id_to_position[cell_id1], cell_id_to_position[cell_id2]] = 1
+    
+    
+    return adjacency_matrix
+
+
+def calculate_mi_tuples(time_series, threshold):
+    
+    def get_mi_tuples(time_series):
+       
+        valid_polygons = time_series.columns
+        n = len(valid_polygons)
+        mi_tuples = []
+        # Loop through each pair of indices (i, j)
+        for i in range(n):
+            for j in range(i + 1, n): 
+                mi_tuples.append((valid_polygons[i], valid_polygons[j]))
+        return mi_tuples
+
+    def iter_polygon_tuples(polygon_tuples, number_of_shuffles, time_series, n_cpu=None):
+        # calculate mi for tuple
+        def loop_wrapper(tuple):
+            #unpack triple of polygon ids
+            x = time_series[tuple[0]].values
+            y = time_series[tuple[1]].values
+            #calculate te and shuffles
+            return tuple, (mutual_information(x,y),
+                          [shuffle_mi(x,y) for i in range(number_of_shuffles)])
+        if n_cpu is None:
+            tuple_mi = {}
+            for tuple in polygon_tuples:
+                tuple_mi[tuple] = loop_wrapper(tuple)[1]
+        else:
+            # note that each job is quite fast, so large chunks help w/ speed
+            with Pool() as pool:
+                tuple_mi = dict(pool.map(loop_wrapper, polygon_tuples, chunksize=200))
+        return tuple_mi
+
+    # Mutual Information
+    def mutual_information(x,y):
+        #joint distribution
+        joint = np.zeros((2,2)) #axis 0: x, axis 1: y
+        joint[0][0] = np.mean((x == 0) & (y == 0))
+        joint[0][1] = np.mean((x == 0) & (y == 1)) 
+        joint[1][0] = np.mean((x == 1) & (y == 0)) 
+        joint[1][1] = np.mean((x == 1) & (y == 1)) 
+        #marginal distributions
+        p_x = np.sum(joint, axis=1)
+        p_y = np.sum(joint, axis=0)
+        #independence distribution
+        p_x_p_y = np.outer(p_x, p_y)
+        #mi calculation
+        np.seterr(divide='ignore', invalid='ignore')
+        mi = np.nansum(joint * np.log2(joint/ p_x_p_y)) #calculate sum only for terms which dont include log(0)
+        return mi
+
+    def shuffle_mi(x,y):
+        #shuffling only one time series enough?
+        y_rand_index = np.random.permutation(y.size)
+        y_shuffled = y[y_rand_index]    
+        res = mutual_information(x, y_shuffled)
+        return res
+
+    # Extract significant tuples and take first 40% most significant one
+    def extract_significant_tuples(mi_values, sig, threshold):
+        significant_tuples = []
+        
+        for tuple, (mi, shuffles) in mi_values.items():
+            # Check if the mean of (mi > shuffles) exceeds the significance level
+            if (mi > shuffles).mean() >= (sig / 100):
+                significant_tuples.append((tuple, mi))  # Append tuple along with its MI value
+        
+        # Sort the significant tuples by MI value in descending order
+        significant_tuples.sort(reverse=True, key=lambda x: x[1])
+        
+        # Calculate the threshold index
+        threshold_index = int(len(significant_tuples) * threshold)
+        
+        # Return the first 'threshold' portion of the sorted significant tuples
+        return [t[0] for t in significant_tuples[:threshold_index]]
+
+    mi_tuples = get_mi_tuples(time_series)
+    mi_values = iter_polygon_tuples(mi_tuples, 100, time_series, n_cpu=None)
+    highest_mi_tuples = extract_significant_tuples(mi_values, sig=95, threshold=threshold)
+    return highest_mi_tuples
+
+
 def get_tuples(neighbor_info_dataframe, time_series, degree):
     
     #first degree connections
     adjacency_matrix = get_adjacency_matrix(neighbor_info_dataframe, time_series)
+    
+    # Convert the adjacency matrix to CSR format for efficient matmul (also done implicitly)
+    adjacency_matrix = adjacency_matrix.tocsr()
+    adjacency_matrix_power = adjacency_matrix.copy()
+
+    #fill connections dict first. keys: (cell1, cell2) values = degree of connection
+    connections = {}
+
+    #higher order connections
+    for d in range(1, degree+1):
+        if d > 1:
+            adjacency_matrix_power = adjacency_matrix_power @ adjacency_matrix
+
+        #remove self loops
+        adjacency_matrix_power.setdiag(0)
+        #get indices of non-zero values
+        rows, cols = adjacency_matrix_power.nonzero()
+
+        for row, col in zip(rows, cols):
+            #add to connections if not already in
+            if (row, col) not in connections:
+                connections[(row, col)] = d
+
+    #return tuples of (cell1, cell2, first degree of connection)
+    tuples = [(neighbor_info_dataframe.index[row], neighbor_info_dataframe.index[col], d) for (row, col), d in connections.items()]
+    
+    return tuples
+
+
+def get_mi_tuples(neighbor_info_dataframe, time_series, degree, mi_threshold):
+    
+    #first degree connections
+    adjacency_matrix = get_adjancency_matrix_mi(neighbor_info_dataframe, time_series, mi_threshold)
     
     # Convert the adjacency matrix to CSR format for efficient matmul (also done implicitly)
     adjacency_matrix = adjacency_matrix.tocsr()
@@ -84,9 +229,6 @@ def get_triples(time_series, neighbor_info_dataframe):
     
     # Remove self loops
     adjacency_matrix.setdiag(0)
-    
-    # Get indices of non-zero values
-    rows, cols = adjacency_matrix.nonzero()
 
     # Convert the adjacency matrix to a dense format for easier manipulation
     dense_adj_matrix = adjacency_matrix.todense()
@@ -108,10 +250,11 @@ def get_triples(time_series, neighbor_info_dataframe):
     # Convert set to list to return the result
     return list(unique_triples)
 
-    
 
 
-def links(time_series, neighbor_info_dataframe, number_of_shuffles, degree, triples):
+
+
+def links(time_series, neighbor_info_dataframe, number_of_shuffles, degree, mi_connections, mi_threshold, triples):
     
     if triples:
         #degree doesnt matter here, only works for tuples
@@ -121,11 +264,15 @@ def links(time_series, neighbor_info_dataframe, number_of_shuffles, degree, trip
         
         return triple_poly_te
     
-    else:
+    elif mi_connections:
+        mi_tuples = get_mi_tuples(neighbor_info_dataframe, time_series, degree, mi_threshold)
+        mi_tuple_poly_te = iter_polygon_pair(mi_tuples, number_of_shuffles, time_series, n_cpu = None)
+        return mi_tuple_poly_te
+    
+    else:   
         tuples = get_tuples(neighbor_info_dataframe, time_series, degree)
-        pair_poly_te = iter_polygon_pair(tuples, number_of_shuffles, time_series)
-        
-    return pair_poly_te
+        pair_poly_te = iter_polygon_pair(tuples, number_of_shuffles, time_series, n_cpu = None)
+        return pair_poly_te
 
 
 def self_links(time_series, number_of_shuffles):
@@ -241,4 +388,4 @@ class CausalGraph(nx.DiGraph):
                 neighbor_dict[node] = neighbor_list_temp
 
         return neighbor_dict
-# end CausalGraph    
+
